5c4a39380b28a0664261d1397e6b281b7f052787
Maruf Hossain <mhtamun@gmail.com>
2023-12-17 01:17:19 +0600

    Fix image link list for next config

--- next.config.js
+++ next.config.js
@@ -12,18 +12,24 @@ const nextConfig = {
             {
                 protocol: 'http',
                 hostname: 'localhost',
-                port: '8082',
+                port: '8081',
                 pathname: '/**',
             },
             {
                 protocol: 'http',
                 hostname: '0.0.0.0',
-                port: '8082',
+                port: '8081',
                 pathname: '/**',
             },
             {
                 protocol: 'https',
                 hostname: 'beta.s2.triphaat.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 's2.triphaat.com',
                 port: '',
                 pathname: '/**',
             },
