c252f59892f75faa749598e7ec83a72191a58342
Maruf Hossain <mhtamun@gmail.com>
2024-01-29 18:56:36 +0600

    Add host to server

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const next = require('next');
 
-const port = parseInt(process.env.PORT) || 8081;
+const host = process.env.HOST || '0.0.0.0';
+const port = parseInt(process.env.PORT) || 8082;
 const dev = process.env.NODE_ENV === 'development';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-    const server = express();
+app.prepare()
+    .then(() => {
+        const server = express();
 
-    server.all('*', (req, res) => handle(req, res));
+        server.all('*', (req, res) => handle(req, res));
 
-    server.listen(port, error => {
-        if (error) throw error;
+        server.listen(port, host, error => {
+            if (error) throw error;
 
-        console.debug(`> Ready on http://0.0.0.0:${port}`);
+            console.debug(`> Ready on http://${host}:${port}`);
+        });
+    })
+    .catch(error => {
+        console.error('server.js', error);
     });
-});
